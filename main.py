680f7bc3e863473b4d6b57d541406d538a61b427
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2022-01-01 22:19:00 +0000

    add data

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import random
 from game_data import data
 
 first_number = random.randint(0, 49)
-name = data[first_number]['name']
-
 print(first_number)
-print(name)
+print(f"{data[first_number]['name']}, {data[first_number]['description']}, from {data[first_number]['country']}")
