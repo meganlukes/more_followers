ace32b2e76c77ea2e7486ab54fa6f135a2445e12
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2022-01-03 13:09:44 +0000

    Fix, couldn't get clear

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
-import random
 from game_data import data
+import random
 
-first_number = random.randint(0, 49)
-print(first_number)
-print(f"{data[first_number]['name']}, {data[first_number]['description']}, from {data[first_number]['country']}")
-opt_a_number = random.randint(0,49)
-opt_b_number = random.randint(0, 49)
-correct = ""
-if data[opt_a_number]['follower_count'] > data[opt_b_number]['follower_count']:
-  correct = "a"
-else:
-  correct = "b"
-points = 0
-player_guess = input("Type 'A' or 'B' ").lower()
-if player_guess == correct:
-  print("Correct!")
-  points = points + 1
-else:
-  print(f"Wrong. Your final score is {points}")
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print("vs")
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
